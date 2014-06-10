2bd60999f9e605d6c83888ee3e7e88aed3989a1d
vocoderism <davide.franzino@gmail.com>
2014-06-10 22:29:25 +0200

    CRoMize

--- crom.mk
+++ crom.mk
@@ -5,8 +5,12 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit device configuration
 $(call inherit-product, device/samsung/toro/full_toro.mk)
 
-# Inherit some common stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/crom/prebuilt/common/media/xhdpi/BOOTANIMATION-1280x768.zip:system/media/bootanimation.zip
+
+# Inherit CRoM common stuff.
+$(call inherit-product, vendor/crom/config/common_phone.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.camera.res=5MP
@@ -17,7 +21,7 @@ PRODUCT_RELEASE_NAME := toro
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysid BUILD_FINGERPRINT="google/mysid/toro:4.2.2/JDQ39/125147:user/release-keys"
 PRIVATE_BUILD_DESC="mysid-user 4.2.2 JDQ39 125147 release-keys"
 
-PRODUCT_NAME := slim_toro
+PRODUCT_NAME := crom_toro
 PRODUCT_DEVICE := toro
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Galaxy Nexus
